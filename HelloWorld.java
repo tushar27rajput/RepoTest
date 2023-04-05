405dba1f9cd0fbc0692a2f4e185f6e305aa87f88
tushar <tushar.rajput774@gmail.com>
2023-04-05 15:31:20 +0530

    updated file

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,9 @@ class HelloWorld
     public static void main(String[] args) 
     {
         System.out.println("Hello, World!"); 
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
     }
 }
